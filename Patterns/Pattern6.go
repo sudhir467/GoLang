c90cd5de397e07be14293c6945c07157911b570c
sudhir467 <sudhirborra6163@gmail.com>
2022-11-15 20:21:36 +0100

    Patterns

--- Patterns/Pattern6.go
+++ Patterns/Pattern6.go
@@ -1,9 +1,9 @@
-/*This program is to print the following pattern
+/* This program is to print the following pattern
 1
-22
-333
-4444
-55555
+12
+123
+1234
+12345
 */
 
 package main
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	var rows, cols, num int
-	fmt.Println("Enter the number of rows required")
+	fmt.Println("Enter the number of rows required:")
 	fmt.Scanf("%d", &num)
 	for rows = 1; rows <= num; rows++ {
 		for cols = 1; cols <= rows; cols++ {
